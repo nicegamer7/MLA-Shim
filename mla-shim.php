4fefcce9a830b1d8aadbc6953da412a26d41a33c
Kermina Awad <kerminaawad@gmail.com>
2021-06-25 13:25:33 -0400

    use four spaces for indentation

--- mla-shim.php
+++ mla-shim.php
@@ -10,39 +10,39 @@
  */
 
 function mla_shim_function($atts) {
-  $a = shortcode_atts(array(
-    'shortcode' => '',
-    'parameters' => '',
-    'values' => '',
-    'content' => '',
-    'delimiter' => '|',
-    'columns' => 0
-  ), $atts);
+    $a = shortcode_atts(array(
+        'shortcode' => '',
+        'parameters' => '',
+        'values' => '',
+        'content' => '',
+        'delimiter' => '|',
+        'columns' => 0
+    ), $atts);
 
-  if (!empty($a['shortcode']) && !empty($a['parameters']) && !empty($a['values'])) {
-    if ($a['columns'] < 0) $a['columns'] = 0;
-    $parameters = explode(',', $a['parameters']);
-    $values = explode(',', $a['values']);
-    $html = '<div class="shim-column">';
+    if (!empty($a['shortcode']) && !empty($a['parameters']) && !empty($a['values'])) {
+        if ($a['columns'] < 0) $a['columns'] = 0;
+        $parameters = explode(',', $a['parameters']);
+        $values = explode(',', $a['values']);
+        $html = '<div class="shim-column">';
 
-    foreach ($values as $i => $val) {
-      if ($a['columns'] != 0 && $i != 0 && $i % $a['columns'] == 0) $html .= '</div><div class="shim-column">';
-      $val = explode($a['delimiter'], $val);
-      $short = "[{$a['shortcode']}";
-      if (!empty($a['content'])) $content = array_pop($val);
+        foreach ($values as $i => $val) {
+            if ($a['columns'] != 0 && $i != 0 && $i % $a['columns'] == 0) $html .= '</div><div class="shim-column">';
+            $val = explode($a['delimiter'], $val);
+            $short = "[{$a['shortcode']}";
+            if (!empty($a['content'])) $content = array_pop($val);
 
-      foreach ($val as $j => $p) {
-        $short .= " {$parameters[$j]}={$p}";
-      }
+            foreach ($val as $j => $p) {
+                $short .= " {$parameters[$j]}={$p}";
+            }
 
-      $short .= ']';
-      if (!empty($content)) $short .= "{$content}[/{$a['shortcode']}]";
-      $html .= do_shortcode($short);
+            $short .= ']';
+            if (!empty($content)) $short .= "{$content}[/{$a['shortcode']}]";
+            $html .= do_shortcode($short);
+        }
+
+        $html .= '</div>';
+        return $html;
     }
-
-    $html .= '</div>';
-    return $html;
-  }
 }
 
 add_shortcode('mla_shim', 'mla_shim_function');
